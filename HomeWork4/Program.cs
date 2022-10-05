19c0ef33bb07ff9fc44cc7fdeff3dfc398e48435
Dmitry Nagovitsyn <nagovicinde@mail.ru>
2022-10-05 21:27:11 +0400

    HomeWork4 added

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N > 1)
+{
+    if (N % 2 == 0)
+    {
+        Console.Write(N + "  ");
+    }
+    N--;
+}
+
+while (N < 1)
+{
+    if (N % 2 == 0)
+    {
+        Console.Write(N + "  ");
+    }
+    N++;
+}
+
+
+
+
+
+
+
+
+//while (N < 1)
+//{
+//    if (N % 2 = 0)
+//    {
+//        Console.WriteLine(N);
+//
+//    }
+//    else (N = N - 1);
+//}
